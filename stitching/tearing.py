88cd8ff7cd0fe29c31fbad9509d7922097e1f87f
Fiete Scheel <fietescheel@gmail.com>
2025-04-25 12:03:24 +0200

    feat: add overlap to tearing script

--- stitching/tearing.py
+++ stitching/tearing.py
@@ -2,10 +2,11 @@ from PIL import Image
 import os
 
 img_dir = "img"
-img_number = "1"
+img_number = "2"
 image_path = f"{img_dir}/{img_number}/original.jpg"
-output_dir = "tiles"
-cols, rows = 6, 4
+output_dir = "tiles_with_overlap" 
+cols, rows = 6, 4                 
+overlap = 5                       
 
 img = Image.open(image_path)
 width, height = img.size
@@ -17,14 +18,13 @@ os.makedirs(f"{img_dir}/{img_number}/{output_dir}", exist_ok=True)
 
 for row in range(rows):
     for col in range(cols):
-        left = col * tile_width
-        upper = row * tile_height
-        right = left + tile_width
-        lower = upper + tile_height
+        left = max(col * tile_width - overlap, 0)
+        upper = max(row * tile_height - overlap, 0)
+        right = min((col + 1) * tile_width + overlap, width)
+        lower = min((row + 1) * tile_height + overlap, height)
 
         tile = img.crop((left, upper, right, lower))
-        tile_name = f"tile_{row}_{col}.jpg"
+        tile_name = f"tile_{row}_{col}_overlap.jpg"
         tile.save(os.path.join(f"{img_dir}/{img_number}/{output_dir}", tile_name))
 
-print("Kacheln wurden gespeichert in:", output_dir)
-
+print("Alle Kacheln wurden gespeichert in:", output_dir)
