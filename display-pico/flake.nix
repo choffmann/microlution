66712f32ffd071d8e2557f52cd676fe974d24f15
Cedrik Hoffmann <dev@choffmann.io>
2025-05-15 06:51:00 +0200

    feat: add first project setup for rp2040

---
+++ display-pico/flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+              inputs.self.overlays.default
+            ];
+          };
+        });
+  in {
+    overlays.default = final: prev: {
+      rustToolchain = final.rust-bin.stable.latest.default.override {
+        extensions = ["rust-src"];
+        targets = ["thumbv6m-none-eabi"];
+      };
+    };
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustToolchain
+          openssl
+          pkg-config
+          cargo-deny
+          cargo-edit
+          cargo-watch
+          rust-analyzer
+
+          # embedded
+          flip-link
+          openocd-rp2040
+          probe-rs-tools
+        ];
+
+        env = {
+          # Required by rust-analyzer
+          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      };
+    });
+  };
+}
