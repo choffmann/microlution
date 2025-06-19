e537a8670d6a2e771db0df0a6cbf72826e13d8a7
Cedrik Hoffmann <dev@choffmann.io>
2025-06-19 12:28:24 +0200

    feat: update deploy script

--- scope-ui/scripts/deploy.sh
+++ scope-ui/scripts/deploy.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-TARGET=aarch64-unknown-linux-gnu
-USER=pi
+HOST=$1
+BIN_PATH="~"
+BIN_NAME="scope-ui"
+BUILD_ARCH=cross-aarch64-linux
+LOG_LEVEL=debug
 
-docker build -t rust-cross-compile .
-docker run --rm --user "$(id -u)":"$(id -g)" -v "$(pwd)":/app -w /app rust-cross-compile cargo build --release --target ${TARGET}
+if ! [ -x "$(command -v nix)" ]; then
+  echo >&2 "Error: nix is not installed"
+  exit 1
+fi
 
-#
-# scp -i ~/.ssh/id_choffmann -r ./target/$TARGET/release/display $USER@$PI_IP:/tmp/
-# ssh -i ~/.ssh/id_choffmann $USER@$PI_IP /tmp/display
+# cleanup
+ssh "$HOST" "rm -rf $BIN_PATH/$BIN_NAME" 
+
+# build
+nix build .#$BUILD_ARCH
+
+# copy
+scp -prq "result/bin/$BIN_NAME" "$HOST:$BIN_PATH" 
+
+# run
+ssh $HOST "RUST_LOG=$LOG_LEVEL $BIN_PATH/$BIN_NAME"
