47ff03d3dc1fbe67f65e1497c9e2effe9676b840
Cedrik Hoffmann <dev@choffmann.io>
2025-06-19 11:17:50 +0200

    feat: update flake to cross compile

--- scope-ui/flake.nix
+++ scope-ui/flake.nix
@@ -3,25 +3,88 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk/master";
+    fenix.url = "github:nix-community/fenix";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs: let
+  outputs = {
+    self,
+    nixpkgs,
+    naersk,
+    fenix,
+    rust-overlay,
+    ...
+  }: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forEachSupportedSystem = f:
-      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+      nixpkgs.lib.genAttrs supportedSystems (system:
         f {
-          pkgs = import inputs.nixpkgs {
+          pkgs = import nixpkgs {
             inherit system;
             overlays = [
-              inputs.rust-overlay.overlays.default
-              inputs.self.overlays.default
+              rust-overlay.overlays.default
+              self.overlays.default
             ];
           };
         });
+
+    buildTargets = {
+      "x86_64-linux" = {
+        crossSystemConfig = "x86_64-unknown-linux-musl";
+        rustTarget = "x86_64-unknown-linux-musl";
+      };
+
+      "aarch64-linux" = {
+        crossSystemConfig = "aarch64-unknown-linux-musl";
+        rustTarget = "aarch64-unknown-linux-musl";
+      };
+
+      # Old Raspberry Pi's
+      "armv6l-linux" = {
+        crossSystemConfig = "armv6l-unknown-linux-musleabihf";
+        rustTarget = "arm-unknown-linux-musleabihf";
+      };
+    };
+
+    eachSystem = supportedSystems: callback:
+      builtins.foldl'
+      (overall: system: overall // {${system} = callback system;})
+      {}
+      supportedSystems;
+
+    eachCrossSystem = supportedSystems: callback:
+      eachSystem supportedSystems (
+        buildSystem:
+          builtins.foldl'
+          (inner: targetSystem:
+            inner
+            // {
+              "cross-${targetSystem}" = callback buildSystem targetSystem;
+            })
+          {default = callback buildSystem buildSystem;}
+          supportedSystems
+      );
+
+    mkPkgs = buildSystem: targetSystem:
+      import nixpkgs ({
+          system = buildSystem;
+        }
+        // (
+          if targetSystem == null
+          then {}
+          else {
+            # The nixpkgs cache doesn't have any packages where cross-compiling has
+            # been enabled, even if the target platform is actually the same as the
+            # build platform (and therefore it's not really cross-compiling). So we
+            # only set up the cross-compiling config if the target platform is
+            # different.
+            crossSystem.config = buildTargets.${targetSystem}.crossSystemConfig;
+          }
+        ));
   in {
     overlays.default = final: prev: {
       rustToolchain = final.rust-bin.stable.latest.default.override {
@@ -50,5 +113,68 @@
         };
       };
     });
+
+    packages =
+      eachCrossSystem
+      (builtins.attrNames buildTargets)
+      (
+        buildSystem: targetSystem: let
+          pkgs = mkPkgs buildSystem null;
+          pkgsCross = mkPkgs buildSystem targetSystem;
+          rustTarget = buildTargets.${targetSystem}.rustTarget;
+
+          fenixPkgs = fenix.packages.${buildSystem};
+
+          mkToolchain = fenixPkgs:
+            fenixPkgs.toolchainOf {
+              channel = "stable";
+              sha256 = "sha256-KUm16pHj+cRedf8vxs/Hd2YWxpOrWZ7UOrwhILdSJBU=";
+            };
+
+          toolchain = fenixPkgs.combine [
+            (mkToolchain fenixPkgs).rustc
+            (mkToolchain fenixPkgs).cargo
+            (mkToolchain fenixPkgs.targets.${rustTarget}).rust-std
+          ];
+
+          buildPackageAttrs =
+            if builtins.hasAttr "makeBuildPackageAttrs" buildTargets.${targetSystem}
+            then buildTargets.${targetSystem}.makeBuildPackageAttrs pkgsCross
+            else {};
+
+          naersk-lib = pkgs.callPackage naersk {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+        in
+          naersk-lib.buildPackage (buildPackageAttrs
+            // rec {
+              src = ./.;
+              strictDeps = true;
+              doCheck = false;
+
+              nativeBuildInputs = [
+                pkgs.perl
+              ];
+
+              OPENSSL_STATIC = "1";
+              OPENSSL_LIB_DIR = "${pkgsCross.pkgsStatic.openssl.out}/lib";
+              OPENSSL_INCLUDE_DIR = "${pkgsCross.pkgsStatic.openssl.dev}/include";
+              TARGET_CC = "${pkgsCross.stdenv.cc}/bin/${pkgsCross.stdenv.cc.targetPrefix}cc";
+
+              CARGO_BUILD_TARGET = rustTarget;
+              CARGO_BUILD_RUSTFLAGS = [
+                "-C"
+                "target-feature=+crt-static"
+
+                "-C"
+                "link-args=-static -latomic"
+
+                # https://github.com/rust-lang/cargo/issues/4133
+                "-C"
+                "linker=${TARGET_CC}"
+              ];
+            })
+      );
   };
 }
