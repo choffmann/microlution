729dd93e7d731e5c151e2c5519173e92a312142b
Julian Müller <julian.mueller77@web.de>
2025-07-06 11:33:19 +0200

    Fix Borders

--- microscope-phx-server/server/lib/server/navigation.ex
+++ microscope-phx-server/server/lib/server/navigation.ex
@@ -6,41 +6,30 @@ defmodule Server.Navigation do
   def move_in_direction(direction, step_size) do
     settings = Settings.get_settings!(1)
 
-    move_in_direction =
-      get_navigate_direction(direction, step_size)
+    move_in_direction = get_navigate_direction_sanga(direction, step_size)
 
-    # Phoenix.PubSub.broadcast(
-    #   Server.PubSub,
-    #   "update-minimap",
-    #   {:update_minimap, direction, step_size}
-    # )
+    Phoenix.PubSub.broadcast(
+      Server.PubSub,
+      "update-minimap",
+      {:update_minimap, direction, step_size}
+    )
 
-    # Settings.update(1, %{
-    #   "current_x" => settings.current_x + move_in_direction.x
-    # })
+    Settings.update(1, %{
+      "current_x" => settings.current_x + move_in_direction.x
+    })
 
-    # Settings.update(1, %{
-    #   "current_y" => settings.current_y + move_in_direction.y
-    # })
+    Settings.update(1, %{
+      "current_y" => settings.current_y + move_in_direction.y
+    })
 
-    case HTTPoison.post(
-           Api.move_to_in_image_coords(),
-           move_in_direction |> Jason.encode!(),
-           [{"Content-Type", "application/json"}]
-         ) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        body
-
-      _ ->
-        []
-    end
+    move_sanga(direction, step_size)
   end
 
   def move_stage(direction, step_size) do
     settings = Settings.get_settings!(1)
 
     move_in_direction =
-      get_navigate_direction(direction, step_size)
+      get_navigate_direction_sanga(direction, step_size)
 
     if move_in_direction.y > 0 or move_in_direction.x > 0 do
       if settings.current_y + move_in_direction.y > settings.boundary_y or
@@ -102,6 +91,66 @@ defmodule Server.Navigation do
     Settings.update(1, %{
       "current_y" => 0
     })
+  end
+
+  def get_navigate_direction_sanga(direction, step_size) do
+    case direction do
+      "up-left" ->
+        %{x: step_size, y: step_size}
+
+      "up" ->
+        %{x: 0, y: step_size}
+
+      "up-right" ->
+        %{x: -step_size, y: step_size}
+
+      "down-left" ->
+        %{x: step_size, y: -step_size}
+
+      "down" ->
+        %{x: 0, y: -step_size}
+
+      "down-right" ->
+        %{x: -step_size, y: -step_size}
+
+      "left" ->
+        %{x: step_size, y: 0}
+
+      "right" ->
+        %{x: -step_size, y: 0}
+    end
+  end
+
+  def move_sanga(direction, step_size) do
+    case direction do
+      "up-left" ->
+        Sanga.Board.safe_move_stage_x(step_size)
+        Sanga.Board.safe_move_stage_y(step_size)
+
+      "up" ->
+        Sanga.Board.safe_move_stage_y(step_size)
+
+      "up-right" ->
+        Sanga.Board.safe_move_stage_x(-step_size)
+        Sanga.Board.safe_move_stage_y(step_size)
+
+      "down-left" ->
+        Sanga.Board.safe_move_stage_x(step_size)
+        Sanga.Board.safe_move_stage_y(-step_size)
+
+      "down" ->
+        Sanga.Board.safe_move_stage_y(-step_size)
+
+      "down-right" ->
+        Sanga.Board.safe_move_stage_x(-step_size)
+        Sanga.Board.safe_move_stage_y(-step_size)
+
+      "left" ->
+        Sanga.Board.safe_move_stage_x(step_size)
+
+      "right" ->
+        Sanga.Board.safe_move_stage_x(-step_size)
+    end
   end
 
   def get_navigate_direction(direction, step_size) do
